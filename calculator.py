132a0a994d3304108143b9bd9bd7de6c59b5df34
graalumj <graalumj@oregonstate.edu>
2021-06-06 11:18:03 -0700

    Linted

--- calculator.py
+++ calculator.py
@@ -12,8 +12,7 @@ def subtract(x, y):
 
 def multiply(x, y):
     return x * y
-    
-    
+
+
 def divide(x, y):
     return x / y
-
--- test_calculator.py
+++ test_calculator.py
@@ -6,7 +6,12 @@ import calculator
 class Test_CalculatorApp:
     def test_add(self):
         assert 5 == calculator.add(2, 3)
-        
+
     def test_subtract(self):
         assert 5 == calculator.subtract(10, 5)
 
+    def test_multiply(self):
+        assert 15 == calculator.multiply(3, 5)
+
+    def test_divide(self):
+        assert 3 == calculator.divide(15, 5)
